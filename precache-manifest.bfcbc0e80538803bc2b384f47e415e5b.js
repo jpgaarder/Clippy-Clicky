487deb32cb5c02cd0cf52cf7d6860f2f32f10096
jpgaarder <quazz4life@gmail.com>
2019-03-31 22:26:04 -0600

    Updates

---
+++ precache-manifest.bfcbc0e80538803bc2b384f47e415e5b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b0933e86c12792454cc2",
+    "url": "/Clippy-Clicky/static/js/runtime~main.40610a30.js"
+  },
+  {
+    "revision": "af310a69cd04d48258b1",
+    "url": "/Clippy-Clicky/static/js/main.e19c66aa.chunk.js"
+  },
+  {
+    "revision": "91415285abecff3cbf27",
+    "url": "/Clippy-Clicky/static/js/2.4fa5e42c.chunk.js"
+  },
+  {
+    "revision": "af310a69cd04d48258b1",
+    "url": "/Clippy-Clicky/static/css/main.7125fdd7.chunk.css"
+  },
+  {
+    "revision": "4824c24c003a89a60333f71e164a3dc8",
+    "url": "/Clippy-Clicky/index.html"
+  }
+];
