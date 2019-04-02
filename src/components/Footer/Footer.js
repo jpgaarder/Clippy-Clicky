a2d94e1369bedfdbba5e68e9e03a798c918a2cd3
jpgaarder <quazz4life@gmail.com>
2019-04-01 18:05:50 -0600

    updating file type

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./Footer.css";
+
+// set the footer to a copyright string
+const Footer = props => (
+  <div className="footer">
+    <p class="text-muted text-center">Clippy fan art does not reflect the views or opinions of the developer, as true as they may seem.</p>
+  </div>
+);
+
+export default Footer;
